dcca7a8cdfeeca8e80180c38903acfe77bc46371
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-07-03 14:38:08 +0200

    frist function

---
+++ project.py
@@ -0,0 +1,11 @@
+#rezervacni system
+
+def nabidka(destinace, ceny):
+    print('We can offer you the following destinations:')
+    print('=' * 80)
+    for i in len(destinace):
+        print('| ' + destinace[i] + ' | ' + ceny[i])
+    print('=' * 80)
+
+def registrace(databaze, udaje):
+    pass
