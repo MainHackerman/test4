0004e0c85abfcee21e7d86873f94dfd8d8fe734b
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-07-03 15:02:08 +0200

    functions update

--- project.py
+++ project.py
@@ -1,17 +1,28 @@
-#rezervacni system
+# rezervacni system
 
 def nabidka(destinace, ceny):
     print('We can offer you the following destinations:')
     print('=' * 80)
-    for i in len(destinace):
+    for i in range(len(destinace)):
         print('| ' + destinace[i] + ' | ' + ceny[i])
     print('=' * 80)
+
 
 def registrace(databaze, udaje):
     pass
 
-def login(jmeno, heslo):
-    pass
 
-def nakup(*destinace):
-    pass
+def login(jmeno, heslo, databaze):
+    for zaznam in databaze:
+        if zaznam[1] == jmeno and zaznam[2] == heslo:
+            return True
+    return False
+
+
+def nakup(destinace, ceny, *vyber):
+    cena = 0
+    for i in range(len(destinace)):
+        for vec in vyber:
+            if destinace[i] == vec:
+                cena += ceny[i]
+    return cena
